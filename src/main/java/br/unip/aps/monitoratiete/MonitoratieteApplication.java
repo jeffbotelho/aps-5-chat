7330a36fda0dfef9b0867dac94ab8aed44086283
Jefferson <jefferson.botelho@aluno.unip.br>
2022-05-14 15:07:11 -0300

    feat: initial commit

---
+++ src/main/java/br/unip/aps/monitoratiete/MonitoratieteApplication.java
@@ -0,0 +1,13 @@
+package br.unip.aps.monitoratiete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoratieteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoratieteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/unip/aps/monitoratiete/MonitoratieteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unip.aps.monitoratiete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoratieteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
